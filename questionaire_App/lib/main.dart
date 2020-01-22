71f0638d85a46d77bf66be6285b91a690446a8f6
anandu R <ananduraj32@gmail.com>
2020-01-23 03:19:22 +0530

    update

--- questionaire_App/lib/main.dart
+++ questionaire_App/lib/main.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-  void answerQuestion() => print('Answer Chosen');
-  @override
-  Widget build(BuildContext context){
-    var questions = ['Was it your choice to do engineering?',
+class MyApp extends StatefulWidget{
+  @override 
+  State<StatefulWidget> createState(){
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp>{
+  var _questionIndex = 0;
+  void _answerQuestion(){
+    setState((){
+      _questionIndex +=1;
+    });
+    print('Answer Chosen $_questionIndex');
+    if(_questionIndex == 3) _questionIndex = 0;
+  }
+  var questions = ['Was it your choice to do engineering?',
                       'What\'s your ambition',
                       'What\'s your favorite color?',]; 
+  @override 
+  Widget build(BuildContext context){
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Anandu R'),    
+            title: Text('Anandu R'),
             ),
           body: Column(
             children: [
-              Text('Question :'),
-              RaisedButton(child: Text('Answer 1'),onPressed: answerQuestion,),
-              RaisedButton(child: Text('Answer 2'),onPressed: answerQuestion,),
-              RaisedButton(child: Text('Answer 3'),onPressed: answerQuestion,),
-              RaisedButton(child: Text('Answer 4'),onPressed: answerQuestion,),
+              Question(questions[_questionIndex]),
+              RaisedButton(child: Text('Answer 1'),onPressed: (){
+                print('Answer 1 has been selected');
+              },),
+              RaisedButton(child: Text('Answer 2'),onPressed: () => print('Answer 2 has been selected')),
+              RaisedButton(child: Text('Answer 3'),onPressed: (){
+                print('Answer 3 has been selected');
+              }),
+              RaisedButton(child: Text('Answer 4'),onPressed: _answerQuestion,),
             ]
           ),
         ),
